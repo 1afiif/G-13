15ccc5b024546bba5c84eb821d132df35c0d583e
iamafif <khayotullokh00057@gmail.com>
2022-09-14 15:12:48 +0900

    change

--- src/Components/Title/index.jsx
+++ src/Components/Title/index.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Title = () => {
   return (
     <div>
-     <h1 style={{textAlign:'center',fontSize:'3em'}}>Lists of Fruit</h1>
+     <h1 style={{textAlign:'center',fontSize:'3em'}}>List of Fruit</h1>
     </div>
   )
 }
