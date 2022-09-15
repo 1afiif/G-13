4779c571839525370800815fd9f44856f4d3a548
iamafif <khayotullokh00057@gmail.com>
2022-09-16 08:39:48 +0900

    added new changes

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { MainContainer } from './style'
+
+const Navbar = () => {
+  return (
+    <MainContainer>
+      
+    </MainContainer>
+  )
+}
+
+export default Navbar
---
+++ src/components/Navbar/style.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+export const MainContainer = styled.div`
+  background: #0d263b; 
+  width: 100vw;
+  height: 8vh;
+`;
