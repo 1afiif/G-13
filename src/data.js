8586e3138ba92a3ed20c6188f228ce6c9b6a93b3
iamafif <khayotullokh00057@gmail.com>
2022-09-16 08:12:43 +0900

    ready to start project

---
+++ src/data.js
@@ -0,0 +1,174 @@
+import Image from '../src/assets/image.png'
+import Image2 from '../src/assets/image1.png'
+import Image3 from '../src/assets/image2.png'
+
+export const data = [
+  {
+    id: 1,
+    img: { Image },
+    info: {
+      name: 'New Apartment Nice View',
+      location: 'Quincy St, Brooklyn, NY, USA',
+      property: {
+        bed: '8',
+        bath: '5',
+        garage: '1',
+        area: '1200 Sq Ft',
+      },
+    },
+  },
+  {
+    id: 2,
+    img: { Image2 },
+    info: {
+      name: 'New Apartment Nice View',
+      location: 'Quincy St, Brooklyn, NY, USA',
+      property: {
+        bed: '10',
+        bath: '5',
+        garage: '1',
+        area: '1200 Sq Ft',
+      },
+    },
+  },
+  {
+    id: 3,
+    img: { Image3 },
+    info: {
+      name: 'New Apartment Nice View',
+      location: 'Quincy St, Brooklyn, NY, USA',
+      property: {
+        bed: '5',
+        bath: '5',
+        garage: '1',
+        area: '1200 Sq Ft',
+      },
+    },
+  },
+  {
+    id: 4,
+    img: { Image },
+    info: {
+      name: 'New Apartment Nice View',
+      location: 'Quincy St, Brooklyn, NY, USA',
+      property: {
+        bed: '4',
+        bath: '5',
+        garage: '1',
+        area: '1200 Sq Ft',
+      },
+    },
+  },
+  {
+    id: 5,
+    img: { Image2 },
+    info: {
+      name: 'New Apartment Nice View',
+      location: 'Quincy St, Brooklyn, NY, USA',
+      property: {
+        bed: '4',
+        bath: '5',
+        garage: '1',
+        area: '1200 Sq Ft',
+      },
+    },
+  },
+  {
+    id: 6,
+    img: { Image },
+    info: {
+      name: 'New Apartment Nice View',
+      location: 'Quincy St, Brooklyn, NY, USA',
+      property: {
+        bed: '4',
+        bath: '5',
+        garage: '1',
+        area: '1200 Sq Ft',
+      },
+    },
+  },
+  {
+    id: 7,
+    img: { Image },
+    info: {
+      name: 'New Apartment Nice View',
+      location: 'Quincy St, Brooklyn, NY, USA',
+      property: {
+        bed: '4',
+        bath: '5',
+        garage: '1',
+        area: '1200 Sq Ft',
+      },
+    },
+  },
+  {
+    id: 8,
+    img: { Image2 },
+    info: {
+      name: 'New Apartment Nice View',
+      location: 'Quincy St, Brooklyn, NY, USA',
+      property: {
+        bed: '4',
+        bath: '5',
+        garage: '1',
+        area: '1200 Sq Ft',
+      },
+    },
+  },
+  {
+    id: 9,
+    img: { Image },
+    info: {
+      name: 'New Apartment Nice View',
+      location: 'Quincy St, Brooklyn, NY, USA',
+      property: {
+        bed: '4',
+        bath: '5',
+        garage: '1',
+        area: '1200 Sq Ft',
+      },
+    },
+  },
+  {
+    id: 10,
+    img: { Image2 },
+    info: {
+      name: 'New Apartment Nice View',
+      location: 'Quincy St, Brooklyn, NY, USA',
+      property: {
+        bed: '4',
+        bath: '5',
+        garage: '1',
+        area: '1200 Sq Ft',
+      },
+    },
+  },
+  {
+    id: 11,
+    img: { Image },
+    info: {
+      name: 'New Apartment Nice View',
+      location: 'Quincy St, Brooklyn, NY, USA',
+      property: {
+        bed: '4',
+        bath: '5',
+        garage: '1',
+        area: '1200 Sq Ft',
+      },
+    },
+  },
+  {
+    id: 12,
+    img: { Image2 },
+    info: {
+      name: 'New Apartment Nice View',
+      location: 'Quincy St, Brooklyn, NY, USA',
+      property: {
+        bed: '4',
+        bath: '5',
+        garage: '1',
+        area: '1200 Sq Ft',
+      },
+    },
+  },
+];
