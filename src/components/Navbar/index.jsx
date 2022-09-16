2ef89e26e849888bfbcbc3cb8f47aef57a6c413d
iamafif <khayotullokh00057@gmail.com>
2022-09-16 12:13:41 +0900

    style change

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
-import { MainContainer } from './style'
+import { IconContainer, MainContainer } from './style'
 
 const Navbar = () => {
   return (
     <MainContainer>
-      
+          <IconContainer>
+              
+      </IconContainer>
     </MainContainer>
   )
 }
--- src/components/Navbar/style.js
+++ src/components/Navbar/style.js
@@ -1,7 +1,15 @@
 import styled from 'styled-components'
+import { ReactComponent as Vector } from '../../assets/Vector.svg'
+import { ReactComponent as Houzing } from '../../assets/Houzing.svg'
 
 export const MainContainer = styled.div`
   background: #0d263b; 
   width: 100vw;
   height: 8vh;
 `;
+
+export const IconContainer = styled.div`
+display: flex;
+align-items: center;
+justify-content: center;
+`
