ed28acf5789513724c480ac212dad7bee9c81fe8
iamafif <khayotullokh00057@gmail.com>
2022-09-22 00:00:00 +0900

    new change

--- src/Edit/index.jsx
+++ src/Edit/index.jsx
@@ -1,10 +1,35 @@
 import React, { Component } from 'react'
-import data from '../data';
+import Data from '../data.js';
 
 class Index extends Component {
+  constructor(props) {
+  super(props);
+  this.state = {
+    fruits:Data,
+  }  
+  }
   render() {
     return (
-      <div>index</div>
+    <div>
+    <table>
+    <thead>
+    <tr>
+    <th>ID</th>
+    <th>Name</th>
+    </tr>           
+    </thead>
+    <tbody>
+        {
+        this.state.fruits.map((value) => (
+         <tr>
+           <th>{value.id}</th>         
+           <th>{value.name}</th>         
+         </tr>
+            ))
+        }
+    </tbody>
+    </table>    
+    </div>
     )
   }
 }
