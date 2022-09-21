4def030c16f407473757f4dc5847afea3d0835a1
iamafif <khayotullokh00057@gmail.com>
2022-09-21 23:51:18 +0900

    new change

---
+++ src/Edit/index.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import data from '../data';
+
+class Index extends Component {
+  render() {
+    return (
+      <div>index</div>
+    )
+  }
+}
+
+export default Index
---
+++ src/data.js
@@ -0,0 +1,10 @@
+const data = [
+   {id:1,name:'Apple'},
+   {id:2,name:'Pineapple'},
+   {id:3,name:'Cherry'},
+   {id:4,name:'Coconut'},
+   {id:5,name:'Banana'},
+];
+
+
+export {data}
