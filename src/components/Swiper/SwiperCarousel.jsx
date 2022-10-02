c7e40c42cc498eb18e914ab554211627ef1c73be
iamafif <khayotullokh00057@gmail.com>
2022-10-02 17:47:32 +0900

    new branch

---
+++ src/components/Swiper/SwiperCarousel.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Container } from './style'
+
+class SwiperCarousel extends Component {
+  render() {
+    return (
+      <Container>
+        
+      </Container>
+    )
+  }
+}
+
+export default SwiperCarousel
