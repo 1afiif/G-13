413981c8c8b32ad19d6f370586c4d2da2afeee20
iamafif <khayotullokh00057@gmail.com>
2022-10-05 16:05:28 +0900

    change

--- src/components/Swiper/SwiperCarousel.jsx
+++ src/components/Swiper/SwiperCarousel.jsx
@@ -4,7 +4,9 @@ import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 import { Autoplay,Navigation, Pagination, Mousewheel, Keyboard } from 'swiper';
-// 4minut 2video
+import Image from '../../assets/img.png'
+import Image2 from '../../assets/img2.png'
+
 class SwiperCarousel extends Component {
   render() {
     return (
@@ -14,24 +16,41 @@ class SwiperCarousel extends Component {
           navigation={true}
           mousewheel={true}
           keyboard={true}
-          pagination={true}
+          pagination={{
+            clickable: true,
+          }}
           autoplay={{
-          delay: 2500,
-          disableOnInteraction: false,
-        }}
-          modules={[Pagination,Navigation,Mousewheel,Keyboard,Autoplay]}
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          modules={[Pagination, Navigation, Mousewheel, Keyboard, Autoplay]}
           className='mySwiper'
         >
-          <SwiperReact.Slide>Slide 1</SwiperReact.Slide>
-          <SwiperReact.Slide>Slide 2</SwiperReact.Slide>
-          <SwiperReact.Slide>Slide 3</SwiperReact.Slide>
-          <SwiperReact.Slide>Slide 4</SwiperReact.Slide>
-          <SwiperReact.Slide>Slide 5</SwiperReact.Slide>
-          <SwiperReact.Slide>Slide 6</SwiperReact.Slide>
-          <SwiperReact.Slide>Slide 7</SwiperReact.Slide>
-          <SwiperReact.Slide>Slide 8</SwiperReact.Slide>
-          <SwiperReact.Slide>Slide 9</SwiperReact.Slide>
+          <SwiperReact.Slide
+            style={{ backgroundImage: `url(${Image})` }}
+          ></SwiperReact.Slide>
+          <SwiperReact.Slide
+            style={{ backgroundImage: `url(${Image2})` }}
+          ></SwiperReact.Slide>
+          <SwiperReact.Slide
+            style={{ backgroundImage: `url(${Image})` }}
+          ></SwiperReact.Slide>
+          <SwiperReact.Slide
+            style={{ backgroundImage: `url(${Image2})` }}
+          ></SwiperReact.Slide>
+          <SwiperReact.Slide
+            style={{ backgroundImage: `url(${Image})` }}
+          ></SwiperReact.Slide>
+          <SwiperReact.Slide
+            style={{ backgroundImage: `url(${Image2})` }}
+          ></SwiperReact.Slide>
         </SwiperReact>
+        <SwiperReact.ButtonWrapper>
+          <SwiperReact.SwiperPrev className='swiper-button-prev' />
+        </SwiperReact.ButtonWrapper>
+        <SwiperReact.ButtonWrapper>
+          <SwiperReact.SwiperNext />
+        </SwiperReact.ButtonWrapper>
       </Container>
     );
   }
--- src/components/Swiper/style.js
+++ src/components/Swiper/style.js
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import {ReactComponent as btnPrev} from '../../assets/prev.svg'
+import {ReactComponent as btnNext} from '../../assets/next.svg'
 
 const Container = styled.div`
   width: 100%;
@@ -7,12 +9,43 @@ const Container = styled.div`
 
 const SwiperReact = styled(Swiper)`
   width: 100%;
+
+  .swiper-button-next::after,.swiper-button-prev::after {
+    font-size: 20px !important;
+    width: 45px !important;
+    height: 45px !important;
+    background: #FFFF;
+    opacity: 0.2;
+    border-radius: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    display: none;
+  }
 `;
 
+SwiperReact.SwiperPrev = styled(btnPrev)`
+   position: absolute;
+   z-index: 5;
+`
+SwiperReact.SwiperNext = styled(btnNext)``
+
+SwiperReact.ButtonWrapper = styled.div`
+    width: 45px;
+    height: 45px;
+    background: #ffff;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    opacity: 0.2;
+    background: red;
+    border-radius: 50%;
+`
+
 SwiperReact.Slide = styled(SwiperSlide)`
-  width: 100%;
   height: 571px;
-  background-color: crimson;
+  background-repeat: no-repeat;
+  background-size: 100% 100%;
 `;
 
 export { Container, SwiperReact };
