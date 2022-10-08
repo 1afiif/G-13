37bc3b5e59af107ef3b8b92af330e0803c6fb3fd
iamafif <khayotullokh00057@gmail.com>
2022-10-08 16:32:33 +0900

    change

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {
+  Houzin,
+  IconContainer,
+  MainContainer,
+  TextContainer,
+  Vecto,
+} from './style';
+
+const Navbar = () => {
+  return (
+    <MainContainer>
+      <IconContainer>
+        <Vecto />
+        <Houzin />
+      </IconContainer>
+      <TextContainer>
+        <p>Home</p>
+        <p>Properties</p>
+        <p>Contacts</p>
+      </TextContainer>
+    </MainContainer>
+  );
+};
+
+export default Navbar;
