8987c86a5a5fbeb033be59b6b8263cf4bdefb488
iamafif <khayotullokh00057@gmail.com>
2022-10-08 16:17:12 +0900

    change

--- src/useEffect/index.jsx
+++ src/useEffect/index.jsx
@@ -1,52 +1,46 @@
-import React,{useState,useEffect} from 'react'
+import React, { useState, useEffect } from 'react';
 import { Container } from './style';
 
 const UseEffect = () => {
-    const [count, setCount] = useState(0);
-    const [data, setData] = useState([]);
-    const [toggle, setToggle] = useState(false);
-    // useEffect(() => {
-    //     console.log('useEffect working 1 ...');
-    //     console.log(toggle,'toggle')
-    // })
+  const [count, setCount] = useState(0);
+  const [data, setData] = useState([]);
+  const [toggle, setToggle] = useState(false);
 
-    // 2 video 31 minute
-    useEffect(() => {
-        console.log('useEffect working 2 ...');
-        fetch('https://jsonplaceholder.typicode.com/users')
-            .then(response => response.json())
-            .then(json => setData(json));
-        console.log(data,'data')
-    }, [toggle]);
-    
-    // useEffect(() => {
-    //     console.log('useEffect working 3 ...');
-    // },[count])
+  useEffect(() => {
+    console.log('useEffect working 2 ...');
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((json) => setData(json));
+    console.log(data, 'data');
+  }, [toggle]);
+
+  useEffect(() => {
+    console.log('useEffect working 3 ...');
+  }, [count]);
   return (
     <Container>
-          <h2>useEffect</h2>
-          <h4>Count: {count}</h4>
-          <button onClick={() => setCount(count + 1)}>+</button>
-          <button onClick={() => setToggle(!toggle)}>Get Data</button>
-          <table border={1}>
-          <thead>
-              <th>ID</th>
-              <th>Name</th>
-              <th>Username</th>
-              <th>Email</th>
-              <th>Phone</th>
-              <th>City</th>
-              <th>Street</th>
-              <th>Zipcode</th>
-              <th>LAT</th>
-              <th>LNG</th>
-              <th>Website</th>
-          </thead>
-          <tbody>
-              {
-              data?.map((value) => {
-               return( 
-               <tr key={value?.id}>
+      <h2>useEffect</h2>
+      <h4>Count: {count}</h4>
+      <button onClick={() => setCount(count + 1)}>+</button>
+      <button onClick={() => setToggle(!toggle)}>Get Data</button>
+      <table border={1}>
+        <thead>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Username</th>
+          <th>Email</th>
+          <th>Phone</th>
+          <th>City</th>
+          <th>Street</th>
+          <th>Zipcode</th>
+          <th>LAT</th>
+          <th>LNG</th>
+          <th>Website</th>
+        </thead>
+        <tbody>
+          {data?.map((value) => {
+            return (
+              <tr key={value?.id}>
                 <td>{value?.id}</td>
                 <td>{value?.name}</td>
                 <td>{value?.username}</td>
@@ -55,17 +49,16 @@ const UseEffect = () => {
                 <td>{value?.address?.city}</td>
                 <td>{value?.address?.street}</td>
                 <td>{value?.address?.zipcode}</td>
-                <td>{value?.address?.geo?.lat}</td> 
-                <td>{value?.address?.geo?.lng}</td> 
-                <td>{value?.website}</td> 
-                </tr>
-                      )
-                  })
-              }
-              </tbody>
-          </table>
+                <td>{value?.address?.geo?.lat}</td>
+                <td>{value?.address?.geo?.lng}</td>
+                <td>{value?.website}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </Container>
-  )
-}
+  );
+};
 
-export default UseEffect
+export default UseEffect;
