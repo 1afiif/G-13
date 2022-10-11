1d40b9e7f6df0a09363303fb2eb8afa6a9f18fe4
iamafif <khayotullokh00057@gmail.com>
2022-10-11 15:52:30 +0900

    change

--- src/UseReducer/index.jsx
+++ src/UseReducer/index.jsx
@@ -1,21 +1,37 @@
 import React,{useReducer} from 'react'
+import {data} from '../data';
 
 const UseReducer = () => {
-    const [state, dispatch] = useReducer((state, action) => {
-        console.log(action);
-        switch (action.type) {
-            case 'plus': return state + 1
-            case 'minus': return state - 1
-            default: return state
-      }
-  },0);
-
+  const [state, dispatch] = useReducer((state, action) => {
+      console.log(state,action)
+  }, {
+    mock:data
+  });
   return (
     <div style={{ flex: 1 }}>
       <h1>UseReducer</h1>
-        <button onClick={() => dispatch({type:'plus', payload:{userId:2, name:'Tillo'}})}>+</button>
-        <h3>Count: {state}</h3>
-        <button onClick={() => dispatch({type:'minus'})}>-</button>
+      <table border={1}>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Status</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          {state.mock.map((value) => {
+            return (
+              <tr key={value.id}>
+                <td>{value?.id}</td>
+                <td>{value?.name}</td>
+                <td>{value?.status}</td>
+                <td><button onClick={() => dispatch({type:'delete',payload:value.id})}>Delete</button></td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,8 @@
+const data = [
+    { id: 1, name: 'Toxir', status: 'Bekorchi' },
+    { id: 2, name: 'Botir', status: 'Biznesmen' },
+    { id: 3, name: 'Shokir', status: 'Taksist' },
+    { id: 4, name: 'Nodir', status: 'Sotuvchi' },
+];
+
+export { data };
