8449622978c2a359afa125f64015ab439a90d505
iamafif <khayotullokh00057@gmail.com>
2022-10-11 23:53:28 +0900

    final change for today

--- src/UseReducer/index.jsx
+++ src/UseReducer/index.jsx
@@ -1,15 +1,39 @@
-import React,{useReducer} from 'react'
-import {data} from '../data';
+import React, { useReducer } from 'react';
+import { data } from '../data';
 
 const UseReducer = () => {
-  const [state, dispatch] = useReducer((state, action) => {
-      console.log(state,action)
-  }, {
-    mock:data
-  });
+  //2video 27minut  
+  const [state, dispatch] = useReducer(
+    (state, action) => {
+      switch (action.type) {
+        //Delete case
+        case 'delete':
+          let result = state.mock.filter(
+            (value) => value.id !== action.payload.userId
+          );
+          return { ...state, mock: result };
+        //Search case
+        case 'search':
+          let searchUser = data?.filter((value) => value?.status?.toLowerCase()?.includes(action?.payload?.eventVal));
+          return {...state, mock:searchUser}
+        default:
+          return state.mock;
+      }
+    },
+    {
+      mock: data,
+    }
+  );
   return (
     <div style={{ flex: 1 }}>
       <h1>UseReducer</h1>
+      <input type="text"
+        placeholder='Search...'
+        onChange={(e) =>
+          dispatch({
+            type: 'search',
+            payload: { eventVal: e.target.value }
+          })} />
       <table border={1}>
         <thead>
           <tr>
@@ -26,7 +50,18 @@ const UseReducer = () => {
                 <td>{value?.id}</td>
                 <td>{value?.name}</td>
                 <td>{value?.status}</td>
-                <td><button onClick={() => dispatch({type:'delete',payload:value.id})}>Delete</button></td>
+                <td>
+                  <button
+                    onClick={() =>
+                      dispatch({
+                        type: 'delete',
+                        payload: { userId: value?.id },
+                      })
+                    }
+                  >
+                    Delete
+                  </button>
+                </td>
               </tr>
             );
           })}
@@ -34,6 +69,6 @@ const UseReducer = () => {
       </table>
     </div>
   );
-}
+};
 
-export default UseReducer
+export default UseReducer;
