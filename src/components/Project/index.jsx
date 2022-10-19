38b35463b0149b14e0eff15296d92f003c9268f9
iamafif <khayotullokh00057@gmail.com>
2022-10-19 17:58:19 +0900

    new

--- src/components/Project/index.jsx
+++ src/components/Project/index.jsx
@@ -1,11 +1,70 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
+import { Btn, BtnWrap, Container, Facebook, InputWrap,Google } from './style'
 
 const Login = () => {
+    const [name, setName] = useState('')
+    const [surname, setSurname] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPass] = useState('')
+    
+    const nameVal = useRef('');
+    const surNameVal = useRef('');
+    const emailVal = useRef('');
+    const passwordVal = useRef('');
+
+    const valueLog = () => {
+        setName(nameVal.current.value )
+        setSurname(surNameVal.current.value )
+        setEmail(emailVal.current.value )
+        setPass(passwordVal.current.value)
+    }
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <Container>
+      <BtnWrap>
+        <Btn color=' #00b300' onClick={() => this.props.changePage('login')}>
+          Login
+        </Btn>
+      </BtnWrap>
+      <InputWrap>
+        <p>Enter your username and password to login.</p>
+        <input ref={nameVal} type='text' placeholder='Name' />
+        <input ref={surNameVal} type='text' placeholder='Surname' />
+        <input ref={emailVal} type='text' placeholder='Email' />
+        <input ref={passwordVal} type='text' placeholder='Password' />
+        <p style={{ color: '#008000', marginLeft: '12em' }}>Forgot Password?</p>
+        <button onClick={valueLog}>Login</button>
+        <h2>
+          <span>Or login with</span>
+        </h2>
+        <button
+          style={{
+            background: 'white',
+            color: '#527a7a',
+            borderColor: '#527a7a',
+            cursor: 'pointer',
+            border: '2px solid #d1e0e0',
+          }}
+        >
+          <Google /> Login with Google
+        </button>
+        <button
+          style={{
+            background: 'white',
+            color: '#527a7a',
+            borderColor: '#527a7a',
+            cursor: 'pointer',
+            border: '2px solid #d1e0e0',
+          }}
+        >
+          <Facebook /> Login with Google
+        </button>
+      <h2 style={{borderBottom:'none',fontSize:'18px'}}>Input Name Value : {name}</h2>
+      <h2 style={{borderBottom:'none',fontSize:'18px'}}>Input Surname Value : {surname}</h2>
+      <h2 style={{borderBottom:'none',fontSize:'18px'}}>Input Email Value : {email}</h2>
+      <h2 style={{borderBottom:'none',fontSize:'18px'}}>Input Password Value : {password}</h2>
+      </InputWrap>
+    </Container>
+  );
+  }
 
 export default Login
--- src/components/Project/style.js
+++ src/components/Project/style.js
@@ -1,1 +1,108 @@
-import styled from "styled-components";
+import styled from 'styled-components';
+import { ReactComponent as Faceboo } from '../../assets/faceboook.svg'
+import { ReactComponent as Googl } from '../../assets/google.svg'
+
+const Facebook = styled(Faceboo)`
+
+`;
+const Google = styled(Googl)``;
+
+const Container = styled.div`
+  width: 45vw;
+  height: auto;
+  margin: 30px auto;
+  padding: 2em;
+`;
+
+const BtnWrap = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.5em;
+  margin-bottom: 2em;
+`;
+const Btn = styled.button`
+  border: none;
+  outline: none;
+  font-size: 24px;
+  cursor: pointer;
+  background: none;
+  color: ${(p) => p.color};
+`;
+const InputWrap = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  h2 {
+    font-size: 14px;
+    width: 55%;
+    text-align: center;
+    border-bottom: 1px solid #d1e0e0;
+    line-height: 0.1em;
+    margin: 10px 0 20px;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+    Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-weight: 400;
+    color :grey;
+  }
+
+  h2 span {
+    background: #fff;
+    padding: 0 10px;
+  }
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+    Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  > p {
+    font-weight: 300;
+    color: #527a7a;
+  }
+  > input {
+    padding-left: 1em;
+    font-weight: 300;
+    color: grey;
+    width: 300px;
+    height: 30px;
+    border: 1px solid #d1e0e0;
+    border-radius: 5px;
+    outline: none;
+    margin-top: 1.3em;
+    :focus {
+      border: 1px solid #008000;
+    }
+  }
+  > button {
+    background: #2eb82e;
+    width: 320px;
+    height: 40px;
+    border: none;
+    color: white;
+    border-radius: 4px;
+    font-size: 17px;
+    margin: 1em 0;
+    :active{
+      transform: scale(0.97);
+    }
+  }
+`;
+const Divider = styled.span`
+  border-left: 1px solid black;
+  height: 30px;
+`;
+
+const LineWrap = styled.div`
+  h2 {
+    width: 100%;
+    text-align: center;
+    border-bottom: 1px solid #000;
+    line-height: 0.1em;
+    margin: 10px 0 20px;
+  }
+
+  h2 span {
+    background: #fff;
+    padding: 0 10px;
+  }
+`;
+
+export { Container, BtnWrap, Btn, Divider, InputWrap,LineWrap,Google,Facebook};
