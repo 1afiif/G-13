e695879aebe5de8fc5e9111f93a76a5ec88cf3c3
iamafif <khayotullokh00057@gmail.com>
2022-10-19 16:58:01 +0900

    last change

--- src/components/Uncontrolled/index.jsx
+++ src/components/Uncontrolled/index.jsx
@@ -1,14 +1,25 @@
 import React, { useRef } from 'react'
 
 const Uncontrolled = () => {
-  const state = useRef('');
-  console.log(state,'uncontrolled');
+  const name = useRef('');
+  const surName = useRef('');
+  const email = useRef('');
+  const password = useRef('');
+  const regBtn = () => {
+   console.log(name.current.value);
+   console.log(surName.current.value);
+   console.log(email.current.value);
+   console.log(password.current.value);
+  }
   return (
     <div style={{ flex: '1' }}>
       <h1>Uncontrolled</h1>
       <div>
-              <input ref={state} type="text" placeholder='Name' />
-              <button>Register</button>
+        <input ref={name} type="text" placeholder='Name' />
+        <input ref={surName} type="text" placeholder='SurName' />
+        <input ref={email} type="text" placeholder='Email' />
+        <input ref={password} type="text" placeholder='Password' />
+        <button onClick={regBtn}>Register</button>
       </div>
     </div>
   );
