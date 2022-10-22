8b332432630bee7f0b50efe74156eaa04fce5b90
iamafif <khayotullokh00057@gmail.com>
2022-10-22 23:12:48 +0900

    new branch finallly initiliazed after long break

---
+++ src/root/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Axios from '../components/Axios'
+import Fetch from '../components/Fetch'
+
+const Root = () => {
+  return (
+    <>
+          <Fetch />
+          <Axios/>
+    </>
+  )
+}
+
+export default Root
