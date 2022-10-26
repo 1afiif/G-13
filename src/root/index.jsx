57cc900f901b4b211f3dbe08c778d4c0e228474c
iamafif <khayotullokh00057@gmail.com>
2022-10-26 17:51:49 +0900

    new lesson

---
+++ src/root/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+// import Home from '../components/Home'
+import Navbar from '../components/Navbar'
+
+const Root = () => {
+  return (
+    <>
+      <Navbar/>
+      {/* <Home/> */}
+    </>
+  )
+}
+
+export default Root
