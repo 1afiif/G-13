24aef6377790b2e6845081a765d7bc9a74ae84a5
iamafif <khayotullokh00057@gmail.com>
2022-10-30 01:10:05 +0900

    change

--- src/root/index.jsx
+++ src/root/index.jsx
@@ -5,6 +5,7 @@ import Home from '../components/Home'
 import Products from '../components/Products'
 import About from '../components/About'
 import Login from '../components/Login'
+import NotFound from '../components/NotFound'
 const Root = () => {
   return (
     <>
@@ -15,7 +16,8 @@ const Root = () => {
         <Route path='/products' element={<Products/>}/>
         <Route path='/about' element={<About/>}/>
         <Route path='/login' element={<Login/>}/>
-       </Route>
+        </Route>
+        <Route path='*' element = {<NotFound/>}/>
       </Routes>
     </>
   );
