35cf37de63183721024fdc88ac38a499ee23d718
iamafif <khayotullokh00057@gmail.com>
2022-11-04 20:56:41 +0900

    change

--- src/root/index.jsx
+++ src/root/index.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Route, Routes, Navigate } from 'react-router-dom'
 import Navbar from '../components/Navbar'
-import Home from '../components/Home'
-import Products from '../components/Products'
-import About from '../components/About'
-import Login from '../components/Login'
+// import Home from '../components/Home'
+// import Products from '../components/Products'
+// import About from '../components/About'
+// import Login from '../components/Login'
 import NotFound from '../components/NotFound'
-import ProductDetail from '../components/ProductDetail'
+// import ProductDetail from '../components/ProductDetail'
+import { navbarData } from '../utils/navbarData'
 
 const Root = () => {
   const token = localStorage.getItem('token');
@@ -14,12 +15,24 @@ const Root = () => {
     <>
       <Routes>
        <Route element={<Navbar/>}>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/home' element={<Home/>}/>
+        {
+            navbarData.map(({ id, path, element, title, isPrivate, hidden }) => {
+              if (!isPrivate) {    
+                return (
+                  <Route key={id} path={path} element={element}/>
+              )
+              } else {
+                return(
+                  <Route key={id} path={path} element={isPrivate ? <Navigate to='/login' /> : element} />
+                )
+              }
+          })
+        }
+        {/* <Route path='/home' element={<Home/>}/>
         <Route path='/products' element={token ? <Products/> : <Navigate to='/login'/>} />
         <Route path='/products/:id' element={<ProductDetail/>} />
         <Route path='/about' element={<About/>}/>
-        <Route path='/login' element={<Login/>}/>
+        <Route path='/login' element={<Login/>}/> */}
         </Route>
         <Route path='*' element = {<NotFound/>}/>
       </Routes>
